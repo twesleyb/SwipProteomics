8f2c0b84232891e87ec78957b06c417e930f1ffa
twesleyb <twesleyb10@gmail.com>
2020-06-17 13:08:39 -0400

    test saving datasets

---
+++ R/convert_to_adjm.R
@@ -0,0 +1,12 @@
+#' convert_to_adjm
+#'
+#' @export
+#'
+
+convert_to_adjm <- function(edges) {
+	suppressPackageStartupMessages({ library(data.table) })
+	adjm <- edges %>% as.data.table() %>% 
+		dcast.data.table(Var1 ~ Var2, value.var = "value") %>% 
+		as.matrix(rownames="Var1")
+	return(adjm)
+}
