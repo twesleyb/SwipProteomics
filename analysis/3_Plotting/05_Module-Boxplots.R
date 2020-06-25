3659ff7d65869d11da77e096390f840ea83827cb
twesleyb <tyler.w.bradshaw@duke.edu>
2020-06-24 20:24:32 -0400

    running plootting code to insure everyghtin is up to date

--- analysis/3_Plotting/05_Module-Boxplots.R
+++ analysis/3_Plotting/05_Module-Boxplots.R
@@ -54,23 +54,32 @@ modules <- modules[order(modules)][-1] # Sort in numerical order.
 # Loop to generate plots.
 plots <- list()
 for (module in modules) {
+
 	# Get data for a given module.
 	module_name <- paste0("M",module)
 	df <- tmt_protein %>% filter(Module == module) %>% 
 		group_by(Module,Genotype,Fraction) %>%
 		summarize(Intensity=sum(Adjusted.Intensity),.groups="drop")
 	df$Genotype <- factor(df$Genotype,levels=c("WT","MUT"))
+
 	# Get module's color.
 	colors <- c(col2hex("gray"), module_colors[module_name])
+
 	# Collect the module's stats.
-	stats <- module_stats %>% filter(Module == module) %>% 
-		select(Module, Nodes, PVE, Hubs, PAdjust)
+	stats <- module_noa %>% filter(Module == paste0("M",module)) %>% 
+		select(Module, `Module Size`,
+		       `Module PC1 PVE`, `Top 3 Hubs`)
+	stats$PAdjust <- module_stats %>% 
+		filter(Module == paste0("M",module)) %>% 
+		select(`PAdjust (Bonferroni)`) %>% unlist()
+
 	# Significance annotations.
 	stats$symbol <- ""
 	if (stats$PAdjust < 0.1) { stats$symbol <- "." }
 	if (stats$PAdjust < 0.05) { stats$symbol <- "*" }
 	if (stats$PAdjust < 0.005) { stats$symbol <- "**" }
 	if (stats$PAdjust < 0.0005) { stats$symbol <- "***" }
+
 	# Generate the plot.
 	plot <- ggplot(df,aes(x=Genotype,y=log2(Intensity),fill=Genotype)) 
 	plot <- plot + geom_boxplot() 
@@ -84,6 +93,7 @@ for (module in modules) {
 	plot <- plot + theme(axis.line.x=element_line())
 	plot <- plot + theme(axis.line.y=element_line())
 	plot <- plot + scale_y_continuous(breaks=scales::pretty_breaks(n=5))
+
 	# Add significance star.
 	if (stats$PAdjust < BF_alpha) {
 		yrange <- range(log2(df$Intensity))
@@ -92,40 +102,37 @@ for (module in modules) {
 			annotate("text",x=1.5,size=7,
 				 y=ypos, label=stats$symbol)
 	}
+
 	## Add a table with module statistics.
 	# Table theme.
-	tab_theme <- ttheme_default()
-	tab_theme$core$fg_params$hjust = 0.5
-	tab_theme$core$bg_params$fill="white"
-	tab_theme$core$bg_params$col=NA
+	#tab_theme <- ttheme_default()
+	#tab_theme$core$fg_params$hjust = 0.5
+	#tab_theme$core$bg_params$fill="white"
+	#tab_theme$core$bg_params$col=NA
+
 	# Create table.
-	idx <-  module_stats$Module == as.character(module)
-	pve <- paste0("PVE=",round(module_stats$PVE[idx],3))
-	padj <- paste0("P-adjust=",round(stats$PAdjust,3))
-	n <- paste0("n Proteins=",module_stats$Nodes[idx])
-	tab <- tableGrob(rbind(padj,pve,n), theme=tab_theme, rows=NULL)
-	# Add border to table.
-	border <- rectGrob(gp = gpar(fill=NA,lwd=2))
-	gtab <- gtable_add_grob(tab, border, 
-				t = 1, 
-				b = nrow(tab), 
-				l = 1, 
-				r  = ncol(tab))
+	#idx <-  module_stats$Module == as.character(module)
+	#pve <- paste0("PVE=",round(module_stats$PVE[idx],3))
+	#padj <- paste0("P-adjust=",round(stats$PAdjust,3))
+	#n <- paste0("n Proteins=",module_stats$Nodes[idx])
+	#gtab <- tableGrob(stats[,!colnames(stats)=="symbol"],
+	#		 theme=ttheme_default(), rows=NULL)
+
 	# Add table to plot.
 	# Should table be positioned on the left or right?
-	min_group <- df %>% ungroup() %>% 
-		filter(Intensity == min(Intensity)) %>% 
-		select(Genotype) %>% unlist() %>%
-		as.character()
-	if (min_group == "WT") {
-		xpos <- 4
-	} else {
-		xpos <- 1
-	}
-	yrange <- range(log2(df$Intensity))
-	ypos <- yrange[1] + 0.15* diff(yrange)
-	plot <- plot + 
-		annotation_custom(gtab, xmin=-Inf,xmax=xpos,ymin=-Inf,ymax=ypos)
+	#min_group <- df %>% ungroup() %>% 
+	#	filter(Intensity == min(Intensity)) %>% 
+	#	select(Genotype) %>% unlist() %>%
+	#	as.character()
+	#if (min_group == "WT") {
+	#	xpos <- 4
+	#} else {
+	#	xpos <- 1
+	#}
+	#yrange <- range(log2(df$Intensity))
+	#ypos <- yrange[1] + 0.15* diff(yrange)
+	#plot <- plot + 
+	#	annotation_custom(gtab, xmin=-Inf,xmax=xpos,ymin=-Inf,ymax=ypos)
 	plots[[module_name]] <- plot
 }
 
@@ -134,5 +141,7 @@ for (module in modules) {
 myfile <- file.path(root,"figs","Modules","Module_Boxplots.pdf")
 ggsavePDF(plots, myfile)
 
-myfile <- file.path(root,"figs","Modules",paste0("Sig",length(sig_modules),"_Module_Boxplots.pdf")
+# Save significant modules.
+myfile <- file.path(root,"figs","Modules",
+		    paste0("Sig",length(sig_modules),"_Module_Boxplots.pdf")
 ggsavePDF(plots[sig_modules], myfile)
