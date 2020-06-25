2578adb455c0bea9d2ac941ce6f557cb9d45525c
twesleyb <tyler.w.bradshaw@duke.edu>
2020-06-25 11:59:45 -0400

    generating network

--- analysis/3_Plotting/Networks/2_Network-Cytoscape-Graph.R
+++ analysis/3_Plotting/Networks/2_Network-Cytoscape-Graph.R
@@ -121,9 +121,8 @@ write_graph(g, myfile, format = "gml")
 # Send to Cytoscape.
 # NOTE: underscores in attribute names are removed. 
 winfile <- gsub("/mnt/d/", "D:/", myfile)
-<<<<<<< HEAD
-cysnetw <- importNetworkFromFile(winfile); Sys.sleep(2)
-unlink(myfile)
+cysnetw <- importNetworkFromFile(winfile)
+Sys.sleep(2); unlink(myfile)
 
 # For REALLY large networks you need to tell cytoscape to create a view of the
 # network. I coded this before...
@@ -137,26 +136,6 @@ result = tryCatch({
 	print("Created Network View!")
 	Sys.sleep(2)
 })
-=======
-cysnetw <- importNetworkFromFile(winfile)
-Sys.sleep(2); unlink(myfile)
-
-# For REALLY large networks you need to tell cytoscape to create a view of the
-# network. I coded this before...
-
-# Check if network view was successfully created.
-if (length(cysnetw)==0) {
-	result = tryCatch({
-		getNetworkViews()
-	}, warning = function(w) {
-		print(w)
-	}, error = function(e) {
-		commandsPOST("view create")
-	}, finally = {
-		print("Created Network View!")
-	})
-} Sys.slee(2)
->>>>>>> dc80f240c117e84f0c43892d3655ed3b3235e4ec
 
 # Visual Property defaults:
 defaults <- list(
@@ -204,26 +183,7 @@ layoutNetwork(netw_layout); Sys.sleep(2); fitContent()
 netw_image <- file.path(figsdir, "Network_Overview")
 winfile <- gsub("/mnt/d/", "D:/", netw_image)
 exportImage(winfile, "svg")
-
-# Convert SVG to tiff.
-<<<<<<< HEAD
-#system(command = paste("svg2tiff",paste0(netw_image,".svg")))
-=======
-system(command = paste("svg2tiff",paste0(netw_image,".svg")))
->>>>>>> dc80f240c117e84f0c43892d3655ed3b3235e4ec
+message("\nConvert svg to tiff before pushing to git!")
 
 # Free up some memory.
 cytoscapeFreeMemory()
-
-<<<<<<< HEAD
-# Save cys session. 
-# NOTE: NETWORK IS TOO BIG!
-#myfile <- file.path(netwdir,"Network_Overview.cys")
-#winfile <- gsub("/mnt/d/","D:/",myfile) 
-#saveSession(winfile)
-=======
-# Save cys session.
-myfile <- file.path(netwdir,"Network_Overview.cys")
-winfile <- gsub("/mnt/d/","D:/",myfile) 
-saveSession(winfile)
->>>>>>> dc80f240c117e84f0c43892d3655ed3b3235e4ec
