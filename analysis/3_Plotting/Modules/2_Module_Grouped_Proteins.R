a805588a4eb12cc2b43326715dffbd534183b2dd
twesleyb <tyler.w.bradshaw@duke.edu>
2020-10-20 17:16:06 -0400

    tried to goback and examine msstats protein summarization

--- analysis/3_Plotting/Modules/2_Module_Grouped_Proteins.R
+++ analysis/3_Plotting/Modules/2_Module_Grouped_Proteins.R
@@ -21,23 +21,7 @@ root = "~/projects/SwipProteomics"
 ## Prepare environment --------------------------------------------------------
 
 renv::load(root,quiet=TRUE)
-
-suppressPackageStartupMessages({
-	library(dplyr)
-	library(ggplot2)
-	library(data.table)
-})
-
 suppressWarnings({ devtools::load_all() })
-
-fontdir <- file.path(root, "fonts")
-figsdir <- file.path(root, "figs", "Modules","Groups")
-
-if (! dir.exists(figsdir)) {
-	dir.create(figsdir,recursive=TRUE)
-}
-
-ggtheme(); set_font("Arial",font_path=fontdir)
 
 # load the data
 data(swip)
@@ -47,8 +31,28 @@ data(sig_modules)
 data(msstats_prot)
 data(module_colors)
 data(msstats_results)
+data(leidenalg_partition) 
 
-washc = gene_map$uniprot[grep("Washc*",gene_map$symbol)]
+# imports
+suppressPackageStartupMessages({
+	library(dplyr)
+	library(ggplot2)
+	library(data.table)
+})
+
+# project dirs
+fontdir <- file.path(root, "fonts")
+figsdir <- file.path(root, "figs", "Modules","Groups")
+if (! dir.exists(figsdir)) {
+	dir.create(figsdir,recursive=TRUE)
+}
+
+# set plotting theme and font
+ggtheme(); set_font("Arial",font_path=fontdir)
+
+# wash complex proteins
+washc_prots = gene_map$uniprot[grep("Washc*",gene_map$symbol)]
+
 
 ## plot protein summary --------------------------------------------------------
 
@@ -110,18 +114,20 @@ plot_protein_summary <- function(protein) {
 ## main ------------------------------------------------------------------------
 
 # Loop to do work.
-#partition <- setNames(rep(1,length(washc)),nm=washc)
-#modules <- split(names(partition),partition)
+partition <- setNames(rep(1,length(washc_prots)),nm=washc_prots)
+modules <- split(names(partition),partition)
 
 modules <- split(names(partition),partition)[-1]
 names(modules) <- paste0("M",names(modules))
+
+module = "M60"
 
 for (module in names(modules)) {
   # Get the modules color
 
   wt_color = "#47b2a4"
-  mut_color <- col2hex("purple")
-  #mut_color <- module_colors[module]
+  #mut_color <- col2hex("purple")
+  mut_color <- module_colors[module]
 
   # 1. Generate all the plots for a given module using plot_protein_summary
   plots <- list()
@@ -142,10 +148,10 @@ for (module in names(modules)) {
   } # EOL for proteins
   close(pbar)
 
-  # combine the data containing the normalized data 
+  # 2. combine the data containing the normalized data 
   # (each protein scaled to maximum within a plot) into a single df
-  df = bind_rows(sapply(plots,"[", "data"))
-  # 2. Generate combined plot.
+  # and generate combined plot
+  df <- bind_rows(sapply(plots,"[", "data"))
   plot <- ggplot(df)
   plot <- plot + aes(x = BioFraction)
   plot <- plot + aes(y = norm_Abundance)
@@ -177,3 +183,74 @@ for (module in names(modules)) {
   ggsavePDF(plot,file=myfile)
   message("\nSaved: ", module)
 } # EOL for modules
+
+
+###############################################################################
+
+# our task is to summarize a module. should this be done for each fraction or
+# each mixture?
+
+# proteins = washc_prots
+subdat <- msstats_prot %>% filter(Protein %in% proteins)
+
+data_list <- subdat %>% group_by(BioFraction) %>% group_split()
+# length == 7
+
+df = data_list[[1]]
+
+#fx = lm(Abundance ~ BioFraction.Genotype + Run)
+myfile <- file.path(root,"rdata","msstats_psm.rda")
+load(myfile) # msstats_psm
+data(leidenalg_partition) # partition
+
+# collect the data
+msstats_psm <- msstats_psm %>% mutate(Module = partition[ProteinName])
+msstats_psm <- msstats_psm %>% filter(!is.na(Module))
+msstats_psm <- msstats_psm %>% filter(!is.na(Intensity))
+# now, for each run...
+#msstats_psm <- msstats_psm %>% mutate(Feature = paste(Mixture,Channel,Module,ProteinName,PeptideSequence, collapse="_"))
+
+
+#################################################
+## another attempt...
+
+# how to summarize module abundance...
+
+fm <- lm(norm_Abundance ~ 0 + Condition + Protein, data = plot$data)
+cf <- as.data.table(coef(fm),keep.rownames="Condition")
+colnames(cf) <- c("Condition", "Fit")
+cf <- cf %>% mutate(Condition = gsub("Condition","", Condition))
+cf <- cf %>% filter(Condition %in% plot$data$Condition)
+
+# combine fit data and plot data
+df = left_join(plot$data,cf,by="Condition")
+
+  plot <- ggplot(df)
+  plot <- plot + aes(x = BioFraction)
+  plot <- plot + aes(y = norm_Abundance)
+  plot <- plot + aes(group = interaction(Protein,Genotype), colour = Genotype, 
+		     shape=Genotype, fill=Genotype,shade=Genotype)
+  plot <- plot + aes(ymin=norm_Abundance - CV)
+  plot <- plot + aes(ymax=norm_Abundance + CV)
+  plot <- plot + geom_line()
+  plot <- plot + geom_ribbon(alpha=0.1, linetype="blank")
+  plot <- plot + geom_point(size=2)
+  plot <- plot + geom_line(aes(x=BioFraction,y=Fit),size=2,colour="red")
+
+
+  plot <- plot + ggtitle(paste0(module," (n = ",length(proteins),")"))
+  plot <- plot + ylab("Normalized Protein Abundance")
+  plot <- plot + scale_y_continuous(breaks=scales::pretty_breaks(n=5))
+  plot <- plot + theme(axis.text.x = element_text(color="black", size=11, 
+						  angle = 0, hjust = 1, 
+						  family = "Arial"))
+  plot <- plot + theme(axis.text.y = element_text(color="black",size=11, 
+						  angle = 0, hjust = 1, 
+						  family = "Arial"))
+  plot <- plot + theme(panel.background = element_blank())
+  plot <- plot + theme(axis.line.x=element_line())
+  plot <- plot + theme(axis.line.y=element_line())
+  plot <- plot + scale_colour_manual(values=c(wt_color,mut_color))
+  plot <- plot + scale_fill_manual(values=c(wt_color,mut_color))
+  plot <- plot + theme(legend.position = "none")
+
