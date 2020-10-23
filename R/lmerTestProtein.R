6d9e76ca1bfdfab9ce8104cca4ea76c262a35bc7
twesleyb <tyler.w.bradshaw@duke.edu>
2020-10-23 16:06:13 -0400

    fixes error in df calculation and correctly uses unscaled variance covar matrix in calculations

--- R/lmerTestProtein.R
+++ R/lmerTestProtein.R
@@ -1,20 +1,21 @@
 #' lmerTestProtein
-#' @import lmerTest
+#' @import dplyr lmerTest
 #' @export lmerTestContrast lmerTestProtein
+
 
 lmerTestContrast <- function(fm, contrast, 
 			     df_prior=0,s2_prior=0) {
 
     # define the comparison to be tested
-    # FIXME: what if contrast is more complicated?
+    stopifnot(sum(as.numeric(contrast))==0)
     pos_group <- names(contrast[contrast==1])
     neg_group <- names(contrast[contrast==-1])
     comparison <- paste(pos_group,neg_group,sep="-")
 
-    # get model's summary and compute degrees of freedom
+    # compute Satterthwaite degrees of freedom
     model_summary <- summary(fm, ddf = "Satterthwaite")
 
-    # collect model df and coefficients
+    # collect model's df and coefficients
     s2_df <- as.numeric(model_summary$coefficients[,"df"][pos_group])
     coeff <- model_summary$coeff[,"Estimate"] # == lme4::fixef(fm)
 
@@ -22,37 +23,25 @@ lmerTestContrast <- function(fm, contrast,
     sigma <- as.numeric(model_summary$sigma) # == stats::sigma(fm)
     theta <- as.numeric(model_summary$optinfo$val) # == lme4::getME(fm, "theta")
 
-    # We need to compute the unscaled variance-covariance matrix!
-    # Obscure, but here it is:
-    #?vcov.merMod -- by inspection we find the calculation of scaled var-covar:
-    #V <- sigm^2 * object@pp$unsc()
-    # fm@pp$unsc is the unscaled variance covariance matrix!
-    #vcov <- model_summary$vcov # == vcov(fm) == fm@vcov_beta
-    unscaled_vcov <- fm@pp$unsc()
-
-    #https://rdrr.io/cran/merDeriv/man/vcov.lmerMod.html
-    # variance of the random effects
-    #lme4::VarCorr(fm,sigma)
-
-    #https://stackoverflow.com/questions/27113456/what-is-the-unscaled-variance-in-rs-linear-model-summary
-    # unscaled: solve(t(X) %*% X)
-    # scaled: solve(t(X) %*% X)*sigma^2 -- scaled by the variance sigma^2!
-
-    # the variation not captured by fixed effects
-    # attr(lme4::VarCorr(fm), "sc")^2
-
-    # extract asymtoptic var-covar matrix from fit model
-    # NOTE: done en route to se2 
-    A <- fm@vcov_varpar
-
-    # vcov_varpara numeric matrix holding the asymptotic variance-covariance 
-    # matrix of the variance parameters (including sigma).
-    # we compute the unmoderated statistics:
-    # things that depend upon the contrast
-    se2 <- as.numeric(contrast %*% unscaled_vcov %*% contrast) # == variance 
-
     # calculate posterior s2
     s2_post <- (s2_prior * df_prior + sigma^2 * s2_df) / (df_prior + s2_df)
+
+    # compute unscaled variance-covariance matrix
+    #?vcov.merMod -- by inspection we find the calculation of scaled var-covar:
+    # >>> V <- sigm^2 * object@pp$unsc()
+    # We can compute the unscaled covar matrix with unsc() accessed within the 
+    # models environment:
+    unscaled_vcov <- fm@pp$unsc()
+
+    # compute scaled variance-covariance matrix
+    vcov <- as.matrix(model_summary$vcov) # == vcov(fm) == fm@vcov_beta
+
+    # compute variance
+    vcov_post <- unscaled_vcov * s2_post
+    variance <- as.numeric(contrast %*% vcov_post %*% contrast)
+
+    # extract asymtoptic var-covar matrix from fit model
+    A <- fm@vcov_varpar
 
     # calculate gradient 
     g <- sapply(fm@Jac_list, function(gm) contrast %*% gm %*% contrast)
@@ -62,17 +51,14 @@ lmerTestContrast <- function(fm, contrast,
     # beta are the mean-value parameters available in fixef(object).
     # given gradient and asymptoptic var-covar
     
-    # calculate denom
+    # given gradient and asy var-covar, compute posterior df
     denom <- as.numeric(t(g) %*% A %*% g)
-
-    # compute posterior df
-    # bayesian? posterior proporional to inverse of - σ² ?
-    #df_post <- (2 *( se2/denom) + df_prior)
+    se2 <- as.numeric(contrast %*% vcov %*% contrast) # == variance 
     df_post <- (se2 / denom) + df_prior
 
     # compute fold change and the t-statistic
     FC <- (contrast %*% coeff)[, 1]
-    t <- FC / sqrt(se2) # 
+    t <- FC / sqrt(variance) # 
 
     # compute the p-value given t-statistic and df.post
     p <- 2 * pt(-abs(t), df = df_post) 
@@ -83,7 +69,7 @@ lmerTestContrast <- function(fm, contrast,
 			     percentControl=2^FC, 
 			     Pvalue=p,
 			     Tstatistic=t, 
-			     SE=sqrt(se2), 
+			     SE=sqrt(variance), 
 			     DF=df_post, 
 			     isSingular=lme4::isSingular(fm))
 
