75b44f92e12036a107cf236c384269f6d3ec5292
twesleyb <tyler.w.bradshaw@duke.edu>
2020-10-26 20:14:36 -0400

    lazy git commit

--- analysis/2_SWIP-TMT/8_module-lmerTest-analysis.R
+++ analysis/2_SWIP-TMT/8_module-lmerTest-analysis.R
@@ -20,13 +20,43 @@ data(msstats_prot)
 # other imports
 suppressPackageStartupMessages({
   library(dplyr)
+  library(ggplot2)
   library(lmerTest)
   library(data.table)
   library(doParallel)
 })
 
 
+## functions
+
+mapGeneIDs <- function(id_in,identifiers,new_ids) {
+  # requires: gene_map
+  # mapping between gene identifiers
+  # prots, uniprot, symbols
+  if (length(id_in)==1) { type = "one" }
+  if (length(id_in)>1) { type = "many" }
+idx <- switch(type,
+	      one = grepl(id_in, gene_map[[identifiers]]),
+	      many = match(id_in,gene_map[[identifiers]]))
+return(gene_map[[new_ids]][idx])
+} #EOF
+
+
+
 ## prepare the data -----------------------------------------------------------
+
+# all modules
+all_modules <- split(names(partition),partition)
+names(all_modules) <- paste0("M",names(all_modules))
+
+# modules -- without M0
+modules <- all_modules[-1]
+
+# data.table describing partition
+part_df <- data.table(Protein = names(partition), Module = paste0("M",partition))
+part_df$Size <- sapply(all_modules,length)[part_df$Module]
+part_df$Symbol <- mapGeneIDs(part_df$Protein,"uniprot","symbol")
+part_df$Entrez <- mapGeneIDs(part_df$Protein,"uniprot","entrez")
 
 # remove any unclustered proteins
 msstats_filt <- msstats_prot %>% filter(Protein %in% names(partition))
@@ -34,35 +64,55 @@ msstats_filt <- msstats_prot %>% filter(Protein %in% names(partition))
 # annotate data with module membership
 msstats_filt$Module <- paste0("M", partition[msstats_filt$Protein])
 
+# summary:
+message("\nNumber of modules : ", length(modules))
+message("\nPercent clustered : ", round(sum(partition!=0)/length(partition),3))
+message("\nMedian module size: ", median(sapply(modules,length)))
+
 
 ## fit WASH complex as an example ----------------------------------------------
 
-# wash prots
-washc <- gene_map$uniprot[grepl("Washc*", gene_map$symbol)]
+# wash complex prots
+washc_prots <- gene_map$uniprot[grepl("Washc*", gene_map$symbol)]
 
-# the formula to be fit:
+
+# wash complex genes
+washc_genes <- sort(mapGeneIDs(washc_prots,"uniprot","symbol"))
+
+
+## the formula to be fit:
 fx1 <- formula(paste(c(
-  "Abundance ~ 0 + Genotype:BioFraction + (1|Mixture)",
-  " + (1|Protein)"
+  "Abundance ~ 0 + Genotype:BioFraction + (1|Mixture) + (1|Protein)"
 ), collapse = " "))
 
-message("\nlmer: ", as.character(fx1)[2], " ~ ", as.character(fx1)[3])
+message("\nlmer fit to WASH complex (", paste(washc_genes,collapse=", "),") proteins:\n",
+	">>>\t",as.character(fx1)[2], " ~ ", as.character(fx1)[3])
 
-# fit the model:
+
+## fit the model:
 fm1 <- lmerTest::lmer(fx1, data = msstats_filt %>% filter(Protein %in% washc))
 
+#plot_profiles(washc_prots)
+
+
+## examine the model
 summary(fm1, ddf = "Satterthwaite")
 
-# goodness of fit
-r.squaredGLMM.merMod(fm1) %>% knitr::kable()
 
-## build a contrast
+## goodness of fit
+r.squaredGLMM.merMod(fm1) %>% knitr::kable()
+message("R2m: Marginal; variation explained by fixed effects.")
+message("R2c: Conditional; total variation explained by the model.")
+
+
+## build a contrast for assessing 'Mutant-Control' comparison
 contrast <- lme4::fixef(fm1)
 contrast[] <- 0
 idx <- which(grepl("Control", names(contrast)))
 contrast[idx] <- -1 / length(idx)
 idy <- which(grepl("Mutant", names(contrast)))
 contrast[idy] <- +1 / length(idy)
+
 
 # asses contrast:
 lmerTestContrast(fm1, contrast) %>%
@@ -74,19 +124,18 @@ lmerTestContrast(fm1, contrast) %>%
 
 
 ## goodness of fit!
-qqnorm(residuals(fm1))
+df <- data.table(x=residuals(fm1))
+plot <- ggplot(df, aes(sample = x)) + stat_qq() + stat_qq_line(col = "red")
+# FIXME: touch up plot
 
 
 ## module level analysis for all modules --------------------------------------
-
-# loop through all modules
-modules <- split(names(partition), partition)[-1]
-names(modules) <- paste0("M", names(modules))
 
 # register parallel backend
 n_cores <- parallel::detectCores() - 1
 doParallel::registerDoParallel(n_cores)
 
+# loop through all modules
 results_list <- foreach(module = names(modules)) %dopar% {
   input <- list(fx1, data = msstats_filt %>% filter(Module == module))
   suppressMessages({ # about boundary fits
@@ -100,7 +149,8 @@ results_list <- foreach(module = names(modules)) %dopar% {
 names(results_list) <- names(modules)
 
 ## collect results
-results_df <- do.call(rbind, results_list)
+results_df <- do.call(rbind, results_list) %>% 
+	as.data.table(keep.rownames = "Module")
 
 # singular results will be removed
 warning(
@@ -108,17 +158,31 @@ warning(
   " modules with singular fits will be removed."
 )
 
+# record singular moduels
+drop <- results_df$Module[results_df$isSingular]
+part_df$isSingular <- part_df$Module %in% drop
+
 # drop singular and perform p.adjust
 results_df <- results_df %>%
   filter(!isSingular) %>%
   arrange(Pvalue) %>%
-  as.data.table(keep.rownames = "Module") %>%
   mutate(
     FDR = p.adjust(Pvalue, method = "BH"),
-    PAdjust = p.adjust(Pvalue, method = "bonferroni")
+    Padjust = p.adjust(Pvalue, method = "bonferroni")
   )
 
-# annotate with module size
+results_df$isSingular <- NULL
+
+k <- unique(results_df$Module)
+m <- modules[k]
+p <- partition
+
+# summary:
+message("\nFinal number of modules : ", length(k))
+message("\nFinal percent clustered : ", round(length(unlist(m))/length(p),3))
+message("\nFinal Median module size: ", median(sapply(modules,length)))
+
+# annotate results with module size
 module_size <- sapply(modules,length)[results_df$Module]
 results_df <- tibble::add_column(results_df,Size=module_size,.after="Module")
 
@@ -126,27 +190,31 @@ results_df <- tibble::add_column(results_df,Size=module_size,.after="Module")
 knitr::kable(head(results_df))
 
 message("Number of significant modules (Bonferroni<0.05): ",
-	sum(results_df$PAdjust<0.05))
+	sum(results_df$Padjust<0.05))
 
 
-# annotate with protein identifiers
+# annotate results with protein identifiers
 results_df$Proteins <- sapply(modules[results_df$Module],paste,collapse=";")
 results_df$Symbols <- sapply(modules[results_df$Module], function(x) {
 	       paste(gene_map$symbol[match(x,gene_map$uniprot)],collapse=";")
 	})
 
 
+# sort columns
+results_df <- results_df %>% 
+	select(Module,Size,Contrast,log2FC,percentControl,
+	       Pvalue,FDR,Padjust,Tstatistic,SE,DF)
+
 # save results as an excel workbook
-df <- data.table(Protein = names(partition), Module = partition)
-results_list <- list("Partition" = df, "Mutant-Control" = results_df)
+results_list <- list("Partition" = part_df, "Mutant-Control" = results_df)
 myfile <- file.path(root, "tables", "S3_Module_Results.xlsx")
 write_excel(results_list, myfile)
 
-# save
+# save module_results as rda
 module_results <- results_df
 myfile <- file.path(root, "data", "module_results.rda")
 save(module_results, file = myfile, version = 2)
 
-# save
+# save formula for module-level contrasts as rda
 myfile <- file.path(root, "data", "fx1.rda")
 save(fx1, file = myfile, version = 2)
