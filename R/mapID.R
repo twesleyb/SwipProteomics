e7fc07f65c920c5390d1cff26458083603c1c5aa
twesleyb <tyler.w.bradshaw@duke.edu>
2020-11-02 09:46:04 -0500

    save modules

---
+++ R/mapID.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+#' @export mapID
+
+mapID <- function(id_in="Washc4",identifiers="symbol",new_ids="uniprot") {
+  # requires: env@gene_map
+  # mapping between gene identifiers:
+  # e.g. mapID('Rogdi', 'symbol', 'uniprot')
+  # maps Rogdi from gene symbol to uniprot ID
+  if (length(id_in)==1) { type = "one" }
+  if (length(id_in)>1) { type = "many" }
+idx <- switch(type,
+	      one = grepl(id_in, gene_map[[identifiers]]),
+	      many = match(id_in,gene_map[[identifiers]]))
+return(gene_map[[new_ids]][idx])
+} #EOF
--- analysis/3_Plotting/Modules/1_Plot-Module-Profiles.R
+++ analysis/3_Plotting/Modules/1_Plot-Module-Profiles.R
@@ -55,26 +55,34 @@ ggtheme(); set_font("Arial",font_path=fontdir)
 
 plot_profile <- function(module, msstats_prot, partition,
 			 module_colors, module_gof, wt_color = "#47b2a4") {
+
+  stopifnot(module %in% module_gof$Module) # run gof script
+
   # Subset
   subdat <- msstats_prot %>% filter(Protein %in% names(partition)) %>% 
 	  mutate(Module=paste0("M",partition[Protein])) %>% 
 	  filter(Module == module)
+
   # number of proteins in module
   nprots <- length(unique(subdat$Protein))
+
   # set factor order (levels)
   subdat$Genotype <- factor(subdat$Genotype,levels= c("Control","Mutant"))
   subdat$BioFraction <- factor(subdat$BioFraction,
 			 levels=c("F4","F5","F6","F7","F8","F9","F10"))
+
   # calculate protein-wise mean of three replicates
   df <- subdat %>% group_by(Protein, Genotype, BioFraction) %>% 
         summarize(mean_Abundance = mean(norm_Abundance), # using norm_Abundance!
 	          SD = sd(norm_Abundance),
 	          N = length(norm_Abundance),
 	          .groups="drop")
+
   # calculate coefficient of variation (CV == unitless error) and scale to max
   df <- df %>% mutate(CV = SD/mean_Abundance)
   df <- df %>% group_by(Protein) %>%
 	mutate(scale_Abundance = mean_Abundance/max(mean_Abundance))
+
   # get module fitted data by fitting linear model to scaled Abundance
   # we do it this way because we want to plot the data w/o batch effect
   fm <- lm(scale_Abundance ~ 0 + Genotype:BioFraction, df)
@@ -82,14 +90,21 @@ plot_profile <- function(module, msstats_prot, partition,
 		       "fit_y" = stats::coef(fm)) %>%
     mutate(Genotype = gsub("Genotype","",sapply(strsplit(coef,"\\:"),"[",1))) %>%
     mutate(BioFraction=gsub("BioFraction","",sapply(strsplit(coef,"\\:"),"[",2)))
+
   # combine module data and fitted values
   df <- left_join(df, fit_df,by=c("Genotype","BioFraction"))
+
   # again, insure factor order is correct
   df$Genotype <- factor(df$Genotype,levels=c("Control","Mutant"))
   df$BioFraction <- factor(df$BioFraction,
 			   levels=c("F4","F5","F6","F7","F8","F9","F10"))
+
   # get r2 for annot plot title
-  r2 <- module_gof %>% filter(Module == module) %>% select(R2.total) %>% as.numeric() 
+  # need to regen stats
+  r2 <- module_gof %>% filter(Module == module) 
+  
+  select(R2.total) %>% as.numeric() 
+
   # Generate the plot
   plot <- ggplot(df)
   plot <- plot + aes(x = BioFraction)
@@ -115,11 +130,14 @@ plot_profile <- function(module, msstats_prot, partition,
   plot <- plot + theme(panel.background = element_blank())
   plot <- plot + theme(axis.line.x=element_line())
   plot <- plot + theme(axis.line.y=element_line())
+
   # add fitted lines
   plot <- plot + geom_line(aes(y=fit_y, group=interaction("fit",Genotype)),
 			   linetype="dashed",alpha=1,size=0.75)
+
   # set colors
   plot <- plot + scale_colour_manual(values=c(wt_color,module_colors[[module]]))
+
   return(plot)
 } #EOF
 
@@ -142,12 +160,14 @@ message("\nGenerating profile plots of ", length(modules), " modules.")
 plot_list <- foreach(module = names(modules)) %dopar% {
 	plot_profile(module, msstats_prot, partition, module_colors, module_gof)
 }
-names(plot_list) <- modules
+names(plot_list) <- names(modules)
 
 
+######################
+# FIXME: why is R2 not there?
 m = sample(names(modules),1)
 plot_list[[m]]
-
+###############
 
 
 # save plots as a single pdf
