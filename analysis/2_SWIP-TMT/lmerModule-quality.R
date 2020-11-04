e72460bafd582d0100b3dc5488d42024d28f82e7
twesleyb <tyler.w.bradshaw@duke.edu>
2020-11-04 18:32:55 -0500

    lazy git commit

--- analysis/2_SWIP-TMT/lmerModule-quality.R
+++ analysis/2_SWIP-TMT/lmerModule-quality.R
@@ -10,12 +10,7 @@ renv::load(root)
 devtools::load_all(root)
 
 # load the data
-data(fx1) # module-level model
-data(swip)
-data(gene_map)
-data(sigprots)
 data(partition)
-data(poor_prots) # proteins with poor fm0 lmer fit (R2<thresh)
 data(msstats_prot)
 
 # imports
@@ -87,8 +82,10 @@ myfile <- file.path(root,"rdata",part_file)
 part_dm <- fread(myfile) %>% as.matrix(rownames="V1") + 1
 part_list <- unlist(apply(part_dm,1,function(x) list(x)),recursive=FALSE)
 
+
 # for a given partition...
-i = 5
+i = sample(length(part_list),1)
+message("\nExamining resolution: ",i)
 partition <- part_list[[i]]
 
 # ... remove modules < 5 nodes
@@ -98,6 +95,7 @@ partition[partition %in% which(table(partition) < 5)] <- 0
 modules <- split(names(partition),partition)
 k <- sum(names(modules) != "M0")
 modules <- setNames(modules,nm=paste0("M",names(modules)))
+message("\nNumber of clusters: ", k)
 
 # ... evaluate its quality
 q_list <- foreach(module = names(modules)) %dopar% { 
@@ -109,19 +107,27 @@ names(q_list) <- names(modules)
 idx <- sapply(q_list,function(x) any(is.na(x))) # drop NA
 q <- unlist(q_list[!idx])
 Q <- sum(q[which(names(q)!="M0")])/k
+message("\nQuality: ", round(Q,5))
 
 
 ###############################################################################
 ## loop to evaluate goodness of fit for all modules defined at each resolution
 ###############################################################################
 
+# register parallel backend
+n_cores <- parallel::detectCores() - 1
+doParallel::registerDoParallel(n_cores)
+
+# loop args
+min_size <- 5
+
 # parallelized nested loop: dopar + do
 t1 <- Sys.time()
 results <- foreach(i = seq(part_list)) %dopar% {
   # for a given partition...
   partition <- part_list[[i]]
-  # ... remove modules < 5 nodes
-  partition[partition %in% which(table(partition) < 5)] <- 0
+  # ... remove modules < min_size nodes
+  partition[partition %in% which(table(partition) < min_size)] <- 0
   # for every module in that partition...
   modules <- split(names(partition),partition)
   k <- sum(names(modules) != "M0")
@@ -130,18 +136,17 @@ results <- foreach(i = seq(part_list)) %dopar% {
   q_list <- foreach(module = names(modules)) %do% { 
  	moduleQuality(module, partition,msstats_prot) }
   names(q_list) <- names(modules)
-  # collect module gof results, exlude NA
-  # FIXME: what about NULL
-  idx <- sapply(q_list,function(x) any(is.na(x))) # drop NA
+  # collect module gof results, exlude non-numeric results
+  idx <- sapply(q_list,function(x) any(!is.numeric(x)))
+  if (sum(idx)>0) { warning(sum(idx),"models removed.") } # null or NA
   q <- unlist(q_list[!idx])
   Q <- sum(q[which(names(q)!="M0")])/k
-  results[[i]] <- Q
+  return(Q)
 }
-difftime(Sys.time(),t1) # 47 seconds to analyze 5 resolutions
+difftime(Sys.time(),t1) 
+# expected time to complete 100 resolutions ~ 20 minutes
 
 ## does this statistic make sense???
-x = unlist(results) # i think it might... basically we want to minimize the 
 # contribution of protein to a modules variance and maximize the variance
 # attributable to the fixed effects (genotype and biofraction).
-
-head(x)
+unlist(results) # i think it might... basically we want to minimize the 
