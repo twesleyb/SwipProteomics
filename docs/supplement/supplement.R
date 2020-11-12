ff01043eb3be002198dd2727e17655a90c31f3c5
twesleyb <tyler.w.bradshaw@duke.edu>
2020-11-12 12:27:25 -0500

    lazy git commit

--- docs/supplement/supplement.R
+++ docs/supplement/supplement.R
@@ -16,7 +16,6 @@ renv::load(root)
 devtools::load_all(root)
 
 suppressPackageStartupMessages({
-  #library(knitr)
   library(dplyr)
   library(data.table)
 })
@@ -45,6 +44,10 @@ summary(fm0, ddf = "Satterthwaite")
 
 ## ---- tab0
 
+library(grid)
+library(gtable)
+library(gridExtra)
+
 df <- summary(fm0, ddf="Satterthwaite")[["coefficients"]] %>%
 	as.data.table(keep.rownames="Coefficient")
 df$Coefficient <- gsub("Genotype|BioFraction","",df$Coefficient)
@@ -52,7 +55,25 @@ colnames(df)[colnames(df)=="Pr(>|t|)"] <- "p value"
 colnames(df)[colnames(df)=="Std. Error"] <- "SE"
 colnames(df)[colnames(df)=="df"] <- "DF"
 df$"p value" <- formatC(df$"p value",digits=3)
-knitr::kable(df)
+df$Estimate <- round(df$Estimate,2)
+df$SE <- round(df$SE,3)
+df$DF <- round(df$DF,2)
+df$"t value" <- round(df$"t value",2)
+tt <- gridExtra::ttheme_default(
+      base_size = 11,
+      core = list(bg_params = list(fill = "white"))
+    )
+tab <- tableGrob(df, rows = NULL, theme = tt)
+# add outside border
+g <- gtable_add_grob(tab, grobs = rectGrob(gp = gpar(fill = NA, lwd = 1)), 
+		     t = 1, b = nrow(tab), l = 1, r = ncol(tab))
+# add rows -- i have no idea how gtable works
+for (i in c(1:nrow(tab))) {
+	g <- gtable_add_grob(g, grobs = rectGrob(gp = gpar(fill = NA, lwd = 1)), 
+		     t = i, b = i, l = 1, r = ncol(tab))
+}
+
+plot(g)
 
 
 ## ---- alt0
