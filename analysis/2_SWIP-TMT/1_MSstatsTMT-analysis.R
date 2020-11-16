d82dce82bd4a72df3aa452328879e7e3d7c0ec75
twesleyb <tyler.w.bradshaw@duke.edu>
2020-11-16 13:00:27 -0500

    lazy git commit

--- analysis/2_SWIP-TMT/1_MSstatsTMT-analysis.R
+++ analysis/2_SWIP-TMT/1_MSstatsTMT-analysis.R
@@ -51,69 +51,8 @@ suppressPackageStartupMessages({
 ## for parallel processing to MSstats::proteinSummarization to speed things up.
 
 
-## Functions ------------------------------------------------------------------
-
-detectOutliers <- function(mixture, psm_df, nbins=5, nSD=4, nComplete=2) {
-
-  # this function identifies QC PSM-level outliers
-  # NOTE: PSM with incomplete (n != nComplete) features are removed
-  # ^we cannot assess reproducibility of a PSM if it was only quantified once
-
-  # collect psm with incomplete (missing) data
-  is_incomplete <- psm_df %>% dplyr::filter(Mixture == mixture & Condition == "Norm") %>%
-	  group_by(PSM) %>% mutate(nObs=length(PSM)) %>% ungroup() %>% filter(nObs != nComplete) %>%
-	  select(PSM) %>% unlist() %>% unique()
-
-  # subset, keep psm with complete features
-  filt_df <- psm_df %>% dplyr::filter(Mixture == mixture & Condition == "Norm") %>%
-	  group_by(PSM) %>% mutate(nObs=length(PSM)) %>% ungroup()  %>%
-          # drop PSM with missing features
-	  filter(nObs == nComplete) %>% 
-	  # calculate mean of log2 Intensity for binning psm
-	  group_by(Mixture,PSM) %>% mutate(meanQC=mean(log2(Intensity))) %>% 
-	  ungroup()
-
-  # group ratio data into intensity bins
-  breaks <- stats::quantile(filt_df$meanQC, seq(0, 1, length.out=nbins+1),
-  		   names=FALSE,na.rm=TRUE)
-  filt_df <- filt_df %>% 
-  	mutate(bin = cut(meanQC, breaks, labels=FALSE, include.lowest=TRUE))
-
-  # compute log ratio (difference) of QC measurments
-  filt_df <- filt_df %>% group_by(Mixture,PSM) %>% 
-  	mutate(ratioQC = diff(log2(Intensity))) %>% ungroup()
-
-  # summarize mean and SD of each bin
-  filt_df <- filt_df %>% group_by(bin) %>% 
-  	mutate(meanRatio = mean(ratioQC,na.rm=TRUE),
-  	       binSD = sd(ratioQC), .groups="drop") %>% ungroup()
-
-  # flag outliers
-  filt_df <- filt_df %>% mutate(isLow = ratioQC < meanRatio - nSD*binSD)
-  filt_df <- filt_df %>% mutate(isHigh = ratioQC > meanRatio + nSD*binSD)
-  filt_df <- filt_df %>% mutate(isOutlier = isLow | isHigh)
-
-  # summary
-  summary_df <- filt_df %>% group_by(bin) %>%
-  	summarize(meanIntensity = mean(Intensity),
-  		  meanRatio = mean(ratioQC,na.rm=TRUE),
-  		  binSD = sd(ratioQC), 
-  		  nOut = sum(isOutlier), .groups="drop")
-
-  # status
-  if (length(is_incomplete) > 0) {
-    warning(length(is_incomplete),
-	    " PSM with incomplete features in mixture ", mixture,
-	    " were removed.", call.=FALSE)
-  }
-
-  out_df <- filt_df %>% select(Mixture,PSM, bin, meanQC, ratioQC, binSD, isOutlier)
-  return(out_df %>% filter(isOutlier))
-} # EOF
-
-
 ## [1] convert to msstats format -----------------------------------------------
-# Proteins with a single feature (i.e. peptide) are removed.
+# Proteins with a single feature (i.e. peptide) are removed if rm_single.
 
 message("\nConverting PD PSM-level data into MSstatsTMT format.")
 
@@ -142,7 +81,7 @@ suppressMessages({ # verbosity
 
 # identify psm outliers for each Mixture
 mix <- c("M1","M2","M3")
-outlier_list <- lapply(mix, detectOutliers, msstats_psm, nbins=5, nSD=4)
+outlier_list <- lapply(mix, rmOutlierPSM, msstats_psm, nbins=5, nSD=4)
 names(outlier_list) <- mix
 
 # all psm outliers
@@ -187,9 +126,9 @@ t0 <- Sys.time()
 
 suppressMessages({ # verbosity
   msstats_prot <- proteinSummarization(filt_psm,
-    method = "msstats",
+    method = "msstats", 
     remove_norm_channel = remove_norm_channel,
-    global_norm = global_norm, # perform global normalization using 'norm'
+    global_norm = global_norm, # perform global norm using 'norm' condition
     MBimpute = MBimpute, 
     reference_norm = reference_norm,
     clusters = n_cores
@@ -232,6 +171,10 @@ suppressWarnings({ # about closing clusters FIXME:
   })
 })
 
+# examine the results
+results1 %>% group_by(Label) %>% 
+	summarize(nSig = sum(adj.pvalue < FDR_alpha)) %>% knitr::kable()
+
 # This takes about 21 minutes for 8.5 k proteins
 message(
   "\nTime to perform 8 'intra-BioFraction' comparisons for ", length(proteins),
@@ -250,6 +193,10 @@ suppressWarnings({ # about closing clusters FIXME:
     )
   })
 })
+
+# examine the results
+results2 %>% group_by(Label) %>% 
+	summarize(nSig = sum(adj.pvalue < FDR_alpha)) %>% knitr::kable()
 
 # This takes about 21 minutes for 8.5 k proteins
 message(
