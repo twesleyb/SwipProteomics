9a9db60af9c4d6aa2c0994c482106c48b8f23b3e
twesleyb <tyler.w.bradshaw@duke.edu>
2020-12-05 10:48:14 -0500

    lazy git commit

--- analysis/1_WASH-iBioID/1_lmTest-BioID-analysis.R
+++ analysis/1_WASH-iBioID/1_lmTest-BioID-analysis.R
@@ -553,3 +553,52 @@ wash_interactome <- bioid_results %>%
 # save as rda
 myfile <- file.path(root,"data","wash_interactome.rda")
 save(wash_interactome, file=myfile, version=2)
+
+
+quit()
+
+## ---- generate noa file for JC
+
+data(bioid_anno)
+
+library(getPPIs)
+data(musInteractome)
+
+wash_interactome
+
+data(bioid_results)
+
+colnames(bioid_results)
+
+anno <- dcast(bioid_anno, Protein ~ Annotation, value.var = "PMID")
+noa <- bioid_results %>% 
+	filter(Protein %in% wash_interactome) %>% 
+	left_join(anno, by="Protein") %>% 
+	mutate(Symbol = toupper(Symbol))
+fwrite(noa, "noa.csv")
+
+
+
+os_keep <- c(9606, 10116, 10090)
+
+data(bioid_gene_map)
+
+entrez <- mapID(wash_interactome,"uniprot","entrez")
+
+# collect interactions between swip and wash_interactome proteins
+wash_ppis <- musInteractome %>% 
+	filter(Interactor_A_Taxonomy %in% os_keep) %>% 
+	filter(Interactor_B_Taxonomy %in% os_keep) %>%
+	subset(osEntrezA %in% entrez & osEntrezB %in% entrez)
+edge_df <- wash_ppis %>% select(osEntrezA, osEntrezB, Publications)
+protA = mapID(edge_df$osEntrezA,'entrez','uniprot')
+protB = mapID(edge_df$osEntrezB,'entrez','uniprot')
+edge_df <- tibble::add_column(edge_df, protA, .before='osEntrezA')
+edge_df <- tibble::add_column(edge_df, protB, .after='protA')
+df1 = edge_df %>% select(protA, protB, Publications)
+df2 = data.table(protA=mapID("Washc1"),
+	   protB=wash_interactome,
+	   Publications='Courtland et al. 2020')
+edge_df = rbind(df1,df2)
+fwrite(edge_df, "edges.csv")
+
