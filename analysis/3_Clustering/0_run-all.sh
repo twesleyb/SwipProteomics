92d6d49c759af50f0b3579f207c7e9cc4927f4d4
twesleyb <tyler.w.bradshaw@duke.edu>
2020-12-08 22:07:59 -0500

    conclude analysis

--- analysis/3_Clustering/0_run-all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Clustering protein covariation network..." && \
-	./1_generate-network.R && \
-	./2_leidenalg-clustering.py && \
-	./3_post-leidenalg.R
--- analysis/4_Module-Analysis/0_run-all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./1_module-lmerTest-analysis.R && \
-	./2-module-variancePartition.R && \
-	./3_module-gsea.R
