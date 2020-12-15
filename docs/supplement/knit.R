b2a834da84d9b5cef68ef5a92fd1ea5137682251
twesleyb <tyler.w.bradshaw@duke.edu>
2020-12-14 20:10:11 -0500

    ignore work

--- docs/supplement/knit.R
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env Rscript
-
-root <- "~/projects/SwipProteomics"
-renv::load(root)
-
-knitr::knit("supplement.Rnw",quiet=TRUE)
--- docs/supplement/knitr_options.R
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env Rscript
-
-root <- "~/projects/SwipProteomics"
-renv::load(root)
-
-cat(str(knitr::opts_chunk$get()))
