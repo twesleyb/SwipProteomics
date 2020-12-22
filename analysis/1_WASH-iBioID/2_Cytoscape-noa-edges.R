d7ce213dc7a3e91085ca8f5b24007e11af94ee41
twesleyb <twesleyb10@gmail.com>
2020-12-22 12:11:42 -0500

    save edge table

--- analysis/1_WASH-iBioID/2_Cytoscape-noa-edges.R
+++ analysis/1_WASH-iBioID/2_Cytoscape-noa-edges.R
@@ -17,57 +17,60 @@ devtools::load_all(root, quiet=TRUE)
 data(bioid_anno)
 data(bioid_results)
 data(bioid_gene_map)
+data(wash_interactome)
 
+library(dplyr)
 library(getPPIs) # twesleyb/getPPIs
 
-data(musInteractome)
+data(musInteractome) # mouse PPIs from HitPredict
 
 
 ## ---- create node annotation (noa) data frame
 
-anno <- dcast(bioid_anno, Protein ~ Annotation, value.var = "PMID")
+anno <- reshape2::dcast(bioid_anno, Protein ~ Annotation, value.var = "PMID")
 
-noa <- bioid_results %>% 
-	filter(Protein %in% wash_interactome) %>% 
-	left_join(anno, by="Protein") %>% 
-	mutate(Symbol = toupper(Symbol))
+noa <- bioid_results %>%
+	filter(Protein %in% wash_interactome) %>%
+	dplyr::left_join(anno, by="Protein") %>%
+	dplyr::mutate(Symbol = toupper(Symbol))
 
+# save
 myfile <- file.path(root,"rdata","noa.csv")
-fwrite(noa, myfile)
+data.table::fwrite(noa, myfile)
 message("saved: ", myfile)
 
 
 ## ---- collect ppis
 
 # keep ppis from human, mouse, and rat
-os_keep <- c(9606, 10116, 10090)
+os_keep <- c(9606, 10116, 10090) # taxonomix identifiers
 
 # map wash interactome uniprot IDs to entrez
 entrez <- mapID(wash_interactome,"uniprot","entrez")
 
 # collect interactions between swip and wash_interactome proteins
-wash_ppis <- musInteractome %>% 
-	filter(Interactor_A_Taxonomy %in% os_keep) %>% 
+wash_ppis <- musInteractome %>%
+	filter(Interactor_A_Taxonomy %in% os_keep) %>%
 	filter(Interactor_B_Taxonomy %in% os_keep) %>%
 	subset(osEntrezA %in% entrez & osEntrezB %in% entrez)
 edge_df <- wash_ppis %>% select(osEntrezA, osEntrezB, Publications)
 
 # entrez IDs to UniProt
-protA = mapID(edge_df$osEntrezA,'entrez','uniprot')
-protB = mapID(edge_df$osEntrezB,'entrez','uniprot')
+protA <- mapID(edge_df$osEntrezA,'entrez','uniprot')
+protB <- mapID(edge_df$osEntrezB,'entrez','uniprot')
 
 # add to table
 edge_df <- tibble::add_column(edge_df, protA, .before='osEntrezA')
 edge_df <- tibble::add_column(edge_df, protB, .after='protA')
 
 # combine as edge data.frame
-df1 = edge_df %>% select(protA, protB, Publications)
-df2 = data.table(protA=mapID("Washc1"),
+df1 <- edge_df %>% select(protA, protB, Publications)
+df2 <- data.table::data.table(protA=mapID("Washc1"),
 	   protB=wash_interactome,
 	   Publications='Courtland et al. 2020')
-edge_df = rbind(df1,df2)
+edge_df <- rbind(df1,df2)
 
 # save
 myfile <- file.path(root,"rdata","edges.csv")
-fwrite(edge_df, myfile)
+data.table::fwrite(edge_df, myfile)
 message("saved: ", myfile)
